3cc77400fed19cc26a13fc57bd37bd85e34faec5
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-11-06 10:27:25 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+
+namespace oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+    }
+}
