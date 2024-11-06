713c2b82076848df4434873d59f4e2d0ce49beea
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-11-06 10:55:48 +0100

    Add files via upload

---
+++ Program til bmi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BMIMonitor.ReferenceTypes;
+
+namespace BMIMonitor
+{
+    class Program_til_bmi
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person
+            {
+                FirstName = "Lukas",
+                LastName = "Haugstad",
+                Weight = 70,
+                Height = 1.78
+            };
+
+            Console.WriteLine($"BMI for {person.FirstName} {person.LastName}: {person.BMI}");
+        }
+    }
+}
